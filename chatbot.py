36a9197e2384d47e458508b432de4d81773fe81f
David Flock <94097817+trackfoo@users.noreply.github.com>
2023-09-05 15:09:31 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,139 +1,28 @@
+from dotenv import load_dotenv
 import os
+import streamlit as st
+import pandas as pd
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
 
-from langchain.document_loaders import PyPDFLoader, TextLoader, CSVLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chat_models import ChatOpenAI
-import chainlit as cl
-from chainlit.types import AskFileResponse
+load_dotenv()
+API_KEY = os.environ['OPENAI_API_KEY']
 
-os.environ["OPENAI_API_KEY"] = '' # Add OpenAI Api Key
+llm = OpenAI(api_token=API_KEY)
+pandas_ai = PandasAI(llm)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 100)
-embeddings = OpenAIEmbeddings()
+st.title('Prompt-driven analysis with PandasAI')
+uploaded_file = st.file_uploader('Upload a CSV file for analysis',type=['csv'])
 
-welcome_message = """Welcome to the room! To get started:
-1. Upload a file (pdf, text or csv).
-2. Ask a question about the file.
-"""
-
-# Feature that chain offers to import a file; 
-def process_file(file: AskFileResponse):
-    import tempfile
-
-    if file.type == "text/plain":
-        Loader = TextLoader
-    elif file.type == "application/pdf":
-        Loader = PyPDFLoader
-    elif file.type == "text/csv":
-        Loader = CSVLoader
-
-    with tempfile.NamedTemporaryFile() as tempfile:
-        tempfile.write(file.content)
-        loader = Loader(tempfile.name)
-        documents = loader.load()
-        docs = text_splitter.split_documents(documents)
-
-        # Label chunks with a source
-        for i, doc in enumerate(docs):
-            doc.metadata["source"] = f"source_{i}"
-        return docs
-
-# Retrieve data from embeddings   
-def get_docsearch(file: AskFileResponse):
-    docs = process_file(file)
-
-    # Save data in the user session
-    cl.user_session.set("docs", docs)
-
-    # Create a unique namespace for the file
-
-    docsearch = Chroma.from_documents(
-        docs, embeddings
-    )
-    return docsearch
-
-# Define on chat start tag
-@cl.on_chat_start
-async def start():
-    # Sending an image with the local file path
-    await cl.Message(content="Welcome!  Use this space to chat with your documents or data.").send()
-    files = None
-    while files is None:
-        files = await cl.AskFileMessage(
-            content = welcome_message,
-            accept=["text/plain","application/pdf","text/csv"], # You can change what type of file you'd like
-            max_size_mb=100,
-            timeout=180,
-        ).send()
-
-    file = files[0]
-
-    msg = cl.Message(content=f"Processing '{file.name}'...")
-    await msg.send()
-
-    # No async implementation in the Pinecone client, fallback to sync
-    docsearch = await cl.make_async(get_docsearch)(file)
-
-    # New chain, unite prompt, LLM, functions and model together
-    chain = RetrievalQAWithSourcesChain.from_chain_type(
-        ChatOpenAI(temperature=0, streaming=True),
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(max_tokens_limit=4097)
-    )
-
-    # Let the user know that the system is ready
-    msg.content = f"'{file.name}' processed.  You can now ask questions!"
-    await msg.update()
-
-    cl.user_session.set("chain",chain)
-
-# Kick off when message sent
-@cl.on_message
-async def main(message):
-    chain = cl.user_session.get("chain") 
-    cb = cl.AsyncLangchainCallbackHandler(
-        stream_final_answer=True, answer_prefix_tokens=["FINAL","ANSWER"]
-    )
-    cb.answer_reached = True
-    res = await chain.acall(message, callbacks=[cb])
-
-    answer = res["answer"]
-    sources = res["sources"].strip()
-    source_elements = []
-
-    # Get the documents from the user session
-    docs = cl.user_session.get("docs")
-    metadatas = [doc.metadata for doc in docs]
-    all_sources = [m["source"] for m in metadatas]
-
-    # Provide citations to user; Helps mitigate hallucination
-    if sources:
-        found_sources = []
-
-        # Add the sources to the message
-        for source in sources.split(","):
-            source_name = source.strip().replace(".","")
-
-            # Get the index of the source
-            try:
-                index = all_sources.index(source_name)
-            except ValueError:
-                continue
-            text = docs[index].page_content
-            found_sources.append(source_name)
-            # Create the text element referenced in the message
-            source_elements.append(cl.Text(content=text,name=source_name))
-
-        if found_sources:
-            answer += f"\nSources: {', '.join(found_sources)}"
-        else:
-            answer += "\nNo sources found"
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head(3))
     
-    if cb.has_streamed_final_answer:
-        cb.final_stream.elements = source_elements
-        await cb.final_stream.update()
-    else:
-        await cl.Message(content=answer, elements=source_elements).send()
+    prompt = st.text_area('Enter your prompt:')
+
+    if st.button('Generate'):
+        if prompt:
+            with st.spinner('Generating response...'):
+                st.write(pandas_ai.run(df,prompt=prompt))
+        else:
+            st.warning('Please enter a prompt.')
